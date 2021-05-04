54b7c3e0a57bb7460badbad5a0c9493330b6917a
Promise Johnson <chiemelapromise30@gmail.com>
2021-05-05 04:43:36 +1300

    Upgrade the code according to the code reviewer

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,8 @@
 module PostsHelper
+    def user_email(post)
+        User.all.find_by_id(post.user_id).email if user_signed_in? 
+    end
+    def display_author
+        'Author' if user_signed_in?
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :posts
+  resources :posts, only: [:new, :create, :index]  
   devise_for :users
+  
   root to: "posts#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
